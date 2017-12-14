f1bd2cf2176c47dbf7e915b9659441ddd5230afb
IceKhan13 <iskandar.sitdikov@gmail.com>
2017-12-14 20:38:06 +0100

    projectPoints

--- src/camera.py
+++ src/camera.py
@@ -5,6 +5,7 @@ class Camera(object):
     """ Camera """
 
     __CALIBRATED_CAMERA_MATRIX_PATH = "src/resources/new_dumps/camera_matrix.npy"
+    __DISTORTION_COEF_PATH = "src/resources/new_dumps/distortion.npy"
     __DISTANCE = 60
 
     def __init__(self):
@@ -12,92 +13,102 @@ class Camera(object):
         self.R = [] # roatation matrix
         self.K = [] # calibration matrix
 
+        self.distortion = [] # distortion coefficents
+
         self.F = [] # fundamential matrix
         self.E = [] # essential matrix
-
+        
         self.__calibrate_camera()
 
     def project(self, obj):
         """ project 3d object into image coordinates """
-        R_t = np.hstack((self.R, self.t))
-        projection_matrix = np.dot(self.K, R_t) 
-
-        return np.dot(projection_matrix, obj)
+        #R_t = np.hstack((self.R, self.t))
+        #projection_matrix = np.dot(self.K, R_t) 
+        #return np.dot(projection_matrix, obj)
+        return cv2.projectPoints(
+            obj,
+            cv2.Rodrigues(self.R)[0],
+            self.t,
+            self.K,
+            self.distortion
+        )[0]
 
     def __calibrate_camera(self):
         """ Loads previously saved calibration matrix from file """
+        self.distortion = np.load(self.__DISTORTION_COEF_PATH)
         self.K = np.load(self.__CALIBRATED_CAMERA_MATRIX_PATH)
 
     def compute_camera_extrinsic(self, image1, image2):
-        # orb = cv2.ORB_create()
+        orb = cv2.ORB_create()
 
-        # # get key points and descriptors
-        # kp1, des1 = orb.detectAndCompute(image1,None)
-        # kp2, des2 = orb.detectAndCompute(image2,None)
+        # get key points and descriptors
+        kp1, des1 = orb.detectAndCompute(image1, None)
+        kp2, des2 = orb.detectAndCompute(image2, None)
 
-        # run matching
-        # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        # matches = bf.match(des1,des2)
-        # matches = sorted(matches, key = lambda x: x.distance)
-        
-        # # select apropriate matches
-        # pts1 = []
-        # pts2 = []
-        # for m in matches[:int(len(matches) * 0.1)]:
-        #     #if m.distance < self.__DISTANCE:
-        #     pts2.append(kp2[m.trainIdx].pt)
-        #     pts1.append(kp1[m.queryIdx].pt)
-        # pts1 = np.int32(pts1)
-        # pts2 = np.int32(pts2)
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        matches = bf.match(des1,des2)
+        matches = sorted(matches, key = lambda x: x.distance)
+        good = matches[:int(len(matches) * 0.1)]
 
-        sift = cv2.xfeatures2d.SIFT_create()
-
-        kp1, des1 = sift.detectAndCompute(image1,None)
-        kp2, des2 = sift.detectAndCompute(image2,None)
-
-        FLANN_INDEX_KDTREE = 0
-        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-        search_params = dict(checks=50)
-
-        flann = cv2.FlannBasedMatcher(index_params,search_params)
-        matches = flann.knnMatch(des1,des2,k=2)
-
-        good = []
+        # select apropriate matches
         pts1 = []
         pts2 = []
-
-        for i,(m,n) in enumerate(matches):
-            if m.distance < 0.8*n.distance:
-                good.append(m)
-                pts2.append(kp2[m.trainIdx].pt)
-                pts1.append(kp1[m.queryIdx].pt)
-
+        for m in matches[:int(len(matches) * 0.1)]:
+            #if m.distance < self.__DISTANCE:
+            pts2.append(kp2[m.trainIdx].pt)
+            pts1.append(kp1[m.queryIdx].pt)
         pts1 = np.int32(pts1)
         pts2 = np.int32(pts2)
 
+        # sift = cv2.xfeatures2d.SIFT_create()
+
+        # kp1, des1 = sift.detectAndCompute(image1,None)
+        # kp2, des2 = sift.detectAndCompute(image2,None)
+
+        # FLANN_INDEX_KDTREE = 0
+        # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        # search_params = dict(checks=50)
+
+        # flann = cv2.FlannBasedMatcher(index_params,search_params)
+        # matches = flann.knnMatch(des1,des2,k=2)
+
+        # good = []
+        # pts1 = []
+        # pts2 = []
+
+        # for i,(m,n) in enumerate(matches):
+        #     if m.distance < 0.6*n.distance:
+        #         good.append(m)
+        #         pts2.append(kp2[m.trainIdx].pt)
+        #         pts1.append(kp1[m.queryIdx].pt)
+
+        # pts1 = np.int32(pts1)
+        # pts2 = np.int32(pts2)
+
         # caluclate fundamential matrix
         F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
-        # E, mask = cv2.findEssentialMat(pts1, pts2, focal=1.0, pp=(486., 265.), method=cv2.RANSAC, prob=0.999, threshold=1.0)
-        #points, R, t, mask = cv2.recoverPose(E, pts1, pts2)
+        E, mask = cv2.findEssentialMat(pts1, pts2, focal=1.0, pp=(486., 265.), method=cv2.RANSAC, prob=0.999, threshold=1.0)
+        points, R, t, mask = cv2.recoverPose(E, pts1, pts2, pp=(486., 265.))
         #points, R, t, mask = cv2.recoverPose(F, pts1, pts2, pp=(486., 265.), mask=mask);
-        E = np.dot(np.dot(self.K.T, F), self.K)
-        points, R, t, mask = cv2.recoverPose(E, pts1, pts2)
+        # E = np.dot(np.dot(self.K.T, F), self.K)
+        # points, R, t, mask = cv2.recoverPose(E, pts1, pts2)
 
-        # SVD
-        U, s, V = np.linalg.svd(E, full_matrices=True)
-        W = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
-        rotation = np.dot(np.dot(U, W.T), V.T)
-        translation = U[:, 2]
+        # # SVD
+        # U, s, V = np.linalg.svd(E, full_matrices=True)
+        # W = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+        # rotation = np.dot(np.dot(U, W.T), V.T)
+        # translation = U[:, 2]
 
         self.E = E
         self.F = F
-        self.R = rotation
-        self.t = np.array([translation]).T
+        self.R = R
+        self.t = t
 
-        print(W)
-
+        
         # NOTE: for debug
         # img3 = cv2.drawMatches(image1,kp1,image2,kp2, good, None, flags=2)
         # cv2.imshow("asdasd", img3)
+
+        #print(self.K)
 
         return E, F, R, t
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from src.camera import Camera
 cv2.ocl.setUseOpenCL(False)
 
 img1 = cv2.imread("img10.png")
-img2 = cv2.imread("img60.png")
+img2 = cv2.imread("img20.png")
 
 height1, width1, _ = img1.shape 
 height2, width2, _ = img2.shape 
@@ -15,11 +15,9 @@ height2, width2, _ = img2.shape
 #img2 = cv2.resize(img2, (int(width2/2), int(height2/2))) 
 
 
-
-
 initial_camera = Camera()
 initial_camera.R = np.eye(3, 3)
-initial_camera.t = np.zeros([3, 1])
+initial_camera.t = np.array([0.0, 0.0, 0.0])
 
 camera = Camera()
 camera.compute_camera_extrinsic(img1, img2)
@@ -37,23 +35,38 @@ def generate_cube(scale=1, shifting=[0, 0, 0]):
     for point in world_coords:
         shifted = [a+b for a, b in zip(point, shifting)]
         scaled = [axis * scale for axis in shifted]
-        scaled.append(1.0)
+        #scaled.append(1.0)
         res.append(np.array(scaled))
 
-    return res
+    return np.array(res)
 
 
 
-for point in generate_cube(0.013, [25, 25, 0]):
-    x, y, _ = initial_camera.project(point)
-    x2, y2, _ = camera.project(point)
+# for point in generate_cube(0.013, [25, 25, 0]):
+#     x, y, _ = initial_camera.project(point)
+#     x2, y2, _ = camera.project(point)
+#     cv2.circle(img1, (int(x), int(y)), 3, (255, 0, 0), thickness=2, lineType=8, shift=0)
+#     cv2.circle(img2, (int(x2), int(y2)), 3, (255, 0, 0), thickness=2, lineType=8, shift=0)
+
+#     print(point)
+#     print("1>>>", x, y)
+#     print("2>>>", x2, y2)
+
+cube = generate_cube(1.0, [0,0,10])
+
+image_points = initial_camera.project(cube)
+image_points2 = camera.project(cube)
+
+print(image_points2)
+for point in image_points2:
+    x, y = point[0]
+    cv2.circle(img2, (int(x), int(y)), 3, (255, 0, 0), thickness=2, lineType=8, shift=0)
+    #print("1>>>", x, y)
+
+for point in image_points:
+    x, y = point[0]
     cv2.circle(img1, (int(x), int(y)), 3, (255, 0, 0), thickness=2, lineType=8, shift=0)
-    cv2.circle(img2, (int(x2), int(y2)), 3, (255, 0, 0), thickness=2, lineType=8, shift=0)
-
-    print(point)
-    print("1>>>", x, y)
-    print("2>>>", x2, y2)
-
+    #print("1>>>", x, y)
 
 while True:
     cv2.imshow("img1",img1)
